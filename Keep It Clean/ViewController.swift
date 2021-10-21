1fa4cbf1d12caf8b57d5e0a85c36c0d029c34efa
Emmanuel Gyekye Atta-Penkra <emmanuel@learnmeja.com>
2021-10-21 15:09:01 -0400

    Initial Commit

---
+++ Keep It Clean/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Keep It Clean
+//
+//  Created by Emmanuel Gyekye Atta-Penkra on 10/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
